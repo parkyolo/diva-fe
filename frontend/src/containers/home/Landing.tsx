07c6bbcaaa920dc3ffa25dcbc516e9b632b551e5
이근학 <o101srb@daum.net>
2024-01-23 20:17:20 +0900

    Refactor: 랜딩페이지 수정 / 캐러셀 이미지 추가 및 버튼 위치 조절

--- frontend/src/containers/home/Landing.tsx
+++ frontend/src/containers/home/Landing.tsx
@@ -1,16 +1,15 @@
 import Header from '@/components/Header';
 import Login from '@/app/login/page';
 import Image from 'next/image';
-import Logo from '@/../public/images/logo.png'
+import Logo from '/public/images/logo.png'
 const Landing = () => {
   return (
     <div>
-      <Header />
-      <main className="border-2 border-black flex-col items-center gap-[50px] self-stretch">
-        <span className="text-center text-white text-[24px]">
-          어느날 디바가 내 폰안으로 들어왓다
+      <main className="flex flex-col items-center gap-[50px] flex-1 self-stretch py-[60px] px-[5px]">
+        <span className="text-center text-white text-[24px] landing">
+          어느날 디바가 내 폰안으로 들어왔다
         </span>
-        <Image src={Logo} width={300} height={348} alt="디바" />
+        <Image src={Logo} width={350} height={350} alt="디바" />
         <Login />
       </main>
     </div>
@@ -18,15 +17,3 @@ const Landing = () => {
 };
 
 export default Landing;
-
-
-// .b {
-//   border: black solid 1px;
-//   display: flex;
-//   padding: 80px 51px;
-//   flex-direction: column;
-//   align-items: center;
-//   gap: 50px;
-//   flex: 1 0 0;
-//   align-self: stretch;
-// }
