e0dba864cb0e478aba9030a28bc55cc9a33acdce
박지영 <20181621@kookmin.ac.kr>
2024-01-24 09:21:30 +0900

    Feat: Navigation 구현

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: false, // useEffect 두 번 호출 방지
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: '@svgr/webpack',
+    });
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
