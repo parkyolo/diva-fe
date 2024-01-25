bdb5f86595939ac0bf4741b5cec69e2b5961657c
박지영 <20181621@kookmin.ac.kr>
2024-01-25 16:19:52 +0900

    Refactor: Landing 페이지 라우팅 방식 수정

--- frontend/src/containers/home/index.tsx
+++ frontend/src/containers/home/index.tsx
@@ -1,12 +1,38 @@
+'use client';
+
 import Content from '@/containers/home/Content';
-import { authOptions } from '@/pages/api/auth/[...nextauth]';
-import { getServerSession } from 'next-auth';
-import Landing from './Landing';
+import Navigation from '@/components/Navigation';
+import { useState } from 'react';
 
-const Home = async () => {
-  const session = await getServerSession(authOptions);
+const Home = () => {
+  const homePage = 0b00;
+  const realMode = 0b01;
+  const tutorialMode = 0b10;
+  const resultPage = 0b11;
 
-  return !session?.user ? <Landing /> : <Content />;
+  const [mode, setMode] = useState(homePage);
+  // const [musicId, setMusicId]
+
+  const handleModeChange = (newMode: number) => {
+    setMode(newMode);
+  };
+
+  return (
+    <>
+      {mode === homePage ? (
+        <>
+          <Content onModeChange={handleModeChange} />
+          <Navigation />
+        </>
+      ) : mode === realMode ? (
+        <>실전</>
+      ) : mode === tutorialMode ? (
+        <>튜토리얼</>
+      ) : (
+        <></>
+      )}
+    </>
+  );
 };
 
 export default Home;
