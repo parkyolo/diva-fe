28ad3e3bf3cd9fbe32a432549e24272a6629757e
cha3088 <cha3088@gmail.com>
2024-01-30 13:12:19 +0900

    Feat: 파이썬 서버 완료

--- python/scores/views.py
+++ python/scores/views.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 from django.shortcuts import render
 from django.db import transaction
@@ -13,7 +14,7 @@ from UltraSingerCustom.src.ScoreSettings import ScoreSettings
 from UltraSingerCustom.src.UltraSinger import UltraSinger
 
 import json
-import chardet
+from unicodedata import normalize
 
 from .model.Score import Score
 
@@ -26,18 +27,12 @@ def calculate_score(request):
 
     # request에서 PracticeResult의 id를 받아온다.
     practice_result_id = str(data['id'])
-    a = bytes(practice_result_id)
-    print(chardet.detect(a)['encoding'])
 
     # request에서 artist를 받아온다.
     artist: str = data['artist']
-    a = bytes(artist)
-    print(chardet.detect(a)['encoding'])
 
     # request에서 title을 받아온다.
     title: str = data['title']
-    a = bytes(title)
-    print(chardet.detect(a)['encoding'])
 
     # 구조는 다음과 같다.
     bucket_name = "diva-s3"
@@ -54,29 +49,28 @@ def calculate_score(request):
     # S3로부터 사용자의 녹음 파일을 다운로드한다.
     # 녹음 파일은 diva-s3/PracticeResult/{practice_result_id}/에 저장된다.
     remote = practice_result_dir + "/" + practice_result_id + "/" + artist + "-" + title + ".mp3"
-    a = bytes(remote)
-    print(chardet.detect(a)['encoding'])
-    print(f'remote: {remote}')
-    compare = "PracticeResult/1/버즈-가시.mp3"
-    a = bytes(compare)
-    print(chardet.detect(a)['encoding'])
-    print(f'compare: {compare}')
 
-    print(f'is equal: {remote == compare}')
+    after_filename = normalize('NFD', remote)
 
-    print([ord(c) for c in remote])
-    print([ord(c) for c in compare])
-
-    bucket.download_file(remote, current_path + "/" + "scores" + "/" + practice_result_dir + "/" + practice_result_id + "/" + artist + "-" + title + ".mp3")
+    bucket.download_file(after_filename,
+                         current_path + "/" + "scores" + "/" + practice_result_dir + "/" + practice_result_id + "/" + artist + "-" + title + ".mp3")
 
     song_dir = "song"
 
     # 녹음 파일을 분석한다.
-    scoreSettings = ScoreSettings(id=practice_result_id, input_file_path=current_path + "/" + "scores" + "/" + song_dir + "/" + artist + "-" + title + "_INFO.txt", input_audio_file_path=current_path + "/" + practice_result_dir + "/" + practice_result_id + "/" + artist + "-" + title + ".mp3", output_file_path=current_path + "/" + "scores" + "/" + practice_result_dir)
+    scoreSettings = ScoreSettings(id=practice_result_id,
+                                  input_file_path=current_path + "/" + "scores" + "/" + song_dir + "/" + artist + "-" + title + "_INFO.txt",
+                                  input_audio_file_path=current_path + "/" + "scores" + "/" + practice_result_dir + "/" + practice_result_id + "/" + artist + "-" + title + ".mp3",
+                                  output_file_path=current_path + "/" + "scores" + "/" + practice_result_dir)
     us = UltraSinger(scoreSettings)
     final_score = us.analyze()
 
-    # 녹음 파일을 S3에 저장한다.
+    # muted 파일을 S3에 저장한다.
+    bucket.upload_file(current_path + "/" + "scores" + "/" + practice_result_dir + "/" + practice_result_id + "/" + artist + "-" + title + "/" + "cache" + "/" + artist + "-" + title + "_mute.wav",
+                       practice_result_dir + "/" + practice_result_id + "/" + artist + "-" + title + "_vocal.wav")
+
+    # PracticeResult에 PracticeResultId 폴더를 지운다.
+    shutil.rmtree(current_path + "/" + "scores" + "/" + practice_result_dir + "/" + practice_result_id)
 
     # 점수를 반환한다.
     score = Score(final_score)
