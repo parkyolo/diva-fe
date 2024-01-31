afa25d2e97ad2dfb06b1092a2e0a0739b75e4e56
이근학 <o101srb@daum.net>
2024-01-31 16:53:12 +0900

    Refactor : 마이페이지 사진 수정

--- frontend/src/containers/mypage/Song/SongItems.tsx
+++ frontend/src/containers/mypage/Song/SongItems.tsx
@@ -10,7 +10,8 @@ const SongItems = ({ song }: ContentProps) => {
     <div className="w-1/3 aspect-square p-2 relative">
       <div className="relative">
         <Image src={song.coverImg} alt={song.title} width={500} height={500} />
-        <span className="w-full text-center absolute top-1/3 ">
+        <div className="absolute top-0 w-full h-full  bg-opacity-45 bg-bg-black "></div>
+        <span className="w-full text-center absolute top-1/2 -translate-y-1/2  text-white">
           {song.title}
         </span>
       </div>
