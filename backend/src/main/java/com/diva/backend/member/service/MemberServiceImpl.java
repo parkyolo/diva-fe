51341d4c497f912f9d8d0a2ed2fb563c85397e74
최이초 <choeyicho1105@gmail.com>
2024-01-31 17:01:58 +0900

    Feat: member 도메인의 DTO 수정

--- backend/src/main/java/com/diva/backend/member/service/MemberServiceImpl.java
+++ backend/src/main/java/com/diva/backend/member/service/MemberServiceImpl.java
@@ -3,7 +3,9 @@ package com.diva.backend.member.service;
 
 import com.diva.backend.member.dto.*;
 import com.diva.backend.member.entity.Member;
+import com.diva.backend.member.entity.VocalRange;
 import com.diva.backend.member.repository.MemberRepository;
+import com.diva.backend.member.repository.VocalRangeRepository;
 import com.diva.backend.post.entity.Post;
 import com.diva.backend.post.entity.PracticeResult;
 import com.diva.backend.post.repository.PostRepository;
@@ -25,16 +27,28 @@ public class MemberServiceImpl implements MemberService {
     private final MemberRepository memberRepository;
     private final PostRepository postRepository;
     private final PracticeResultRepository practiceResultRepository;
+    private final VocalRangeRepository vocalRangeRepository;
 
+    @Transactional
     @Override
     public MemberResponseDto getMemberInfo(String email) {
         Member member = memberRepository.findMemberByEmail(email)
             // NULL일 경우 exception 처리
             .orElseThrow(() -> new RuntimeException("해당하는 회원 없음"));
+        VocalRange vocalRange = member.getVocalRange();
+//        VocalRange vocalRange = vocalRangeRepository.findByMemberId(member.getId());
+
+        VocalRangeDto vocalRangeDto = null;
+        if (vocalRange != null) {
+            vocalRangeDto = VocalRangeDto.from(vocalRange);
+        }
+
         return MemberResponseDto.builder()
             .nickname(member.getNickname())
             .email(member.getEmail())
             .profileImg(member.getProfileImg())
+            .vocalRange(vocalRangeDto)
+            .gender(member.getGender())
             .build();
     }
 
