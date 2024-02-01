2abe6d4a49b6bb7dd4622ca47d89acc7a1251f44
박지영 <20181621@kookmin.ac.kr>
2024-02-01 16:27:03 +0900

    Feat: 가사 파일 수정 및 싱크

--- frontend/src/utils/convertBeat2Seconds.ts
+++ frontend/src/utils/convertBeat2Seconds.ts
@@ -1,3 +1,5 @@
+const delay = 0.5;
+
 export const convertBeat2Seconds = (bpm: number, gap: number, beat: number) => {
-  return (beat * 60) / bpm + gap;
+  return (beat * 60) / bpm + gap - delay;
 };
