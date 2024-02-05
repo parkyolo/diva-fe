d235b399a8af83f2113b6ab4789e383f7d54ae89
cha3088 <cha3088@gmail.com>
2024-02-05 15:21:24 +0900

    Fix: Redis 접근 수정

--- python/scores/middleware/SingleRequestOnlyMiddleware.py
+++ python/scores/middleware/SingleRequestOnlyMiddleware.py
@@ -1,22 +1,27 @@
-import redis
 from django.conf import settings
 from django.http import HttpResponse
 
+import django_redis
 
 class SingleRequestOnlyMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-        self.redis = redis.StrictRedis(host='i10a607.p.ssafy.io', port=3307, db=0)
+        self.redis = django_redis.get_redis_connection('default')
 
     def __call__(self, request):
         redis_key = settings.SHARED_REDIS_KEY
 
         # redis에 API가 처리 중인지 확인
         if self.redis.exists(redis_key):
+            print("API 처리 중")
+
+            print(self.redis.get(redis_key))
+
             # 처리 중이면 429 http status code 반환
-            return HttpResponse('{"error": "API is currently processing a request. Please try again later."}', status=429)
+            return HttpResponse('{"error": "API is currently procesing a request. Please try again later."}', status=429)
 
         # redis에 API가 처리 중이라는 key 생성
+        print("redis에 API가 처리 중이라는 key 생성")
         self.redis.set(redis_key, 1)
 
         # request 처리
