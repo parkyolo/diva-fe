ba6463d3361004af77ad0f6df004b7c8d4241b61
이수지 <suzy0120@naver.com>
2024-02-05 10:15:21 +0900

    Feat: 전체 게시글 보기 기능 구현중

--- backend/src/main/java/com/diva/backend/post/service/PostService.java
+++ backend/src/main/java/com/diva/backend/post/service/PostService.java
@@ -1,9 +1,15 @@
 package com.diva.backend.post.service;
 
 import com.diva.backend.post.dto.PostCreateResponseDto;
+import com.diva.backend.post.dto.PostSelectResponseDto;
 import com.diva.backend.post.dto.PostUpdateRequestDto;
 
+import java.util.List;
+
 public interface PostService {
+    // 게시글 전체 조회
+    List<PostSelectResponseDto> getAllPosts();
+
     // 게시글 작성
     void createPost(Long memberId, String content, Long practiceResultId);
 
