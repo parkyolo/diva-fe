b7af15a527ed451f84f1db95aced00c52775ed02
차명훈 <cha3088@gmail.com>
2024-02-06 09:54:01 +0900

    Fix: Song, SongRange, VocalRange, ArtistInfo 수정

--- backend/src/main/java/com/diva/backend/song/entity/Song.java
+++ backend/src/main/java/com/diva/backend/song/entity/Song.java
@@ -1,20 +1,26 @@
 package com.diva.backend.song.entity;
 
+import static jakarta.persistence.GenerationType.IDENTITY;
+
 import com.diva.backend.entity.BaseEntity;
 import com.diva.backend.post.entity.PracticeResult;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
-import static jakarta.persistence.GenerationType.IDENTITY;
+import lombok.Setter;
 
 @Entity
 @Getter
@@ -43,10 +49,11 @@ public class Song extends BaseEntity {
     private String lyric;
 
     @NotBlank
-    @Column(name = "mr_url", length = 200)
+    @Column(name = "mr_url", length = 300)
     private String mrUrl;
 
-    @NotNull
+//    @NotNull
+    @Setter
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "song_range_id")
     private SongRange songRange;
