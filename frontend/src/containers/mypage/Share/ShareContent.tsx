47c23c781e567f2de6b89190c8c5c818d41c740c
이근학 <o101srb@daum.net>
2024-02-07 15:47:11 +0900

    Feat: 공유한 노래 API

--- frontend/src/containers/mypage/Share/ShareContent.tsx
+++ frontend/src/containers/mypage/Share/ShareContent.tsx
@@ -4,11 +4,10 @@ import { useEffect } from 'react';
 import { useFetch } from '@/hooks/useFetch';
 import { req } from '@/services';
 
-
 const ShareContent = () => {
-  const [isLoading, sharedSongs, error, getSharedSongs] = useFetch<SharedSong[]>(
-    req.post.getMyPosts,
-  );
+  const [isLoading, sharedSongs, error, getSharedSongs] = useFetch<
+    SharedSong[]
+  >(req.post.getMyPosts);
 
   useEffect(() => {
     getSharedSongs();
@@ -16,7 +15,7 @@ const ShareContent = () => {
 
   return (
     <>
-      <div className="flex flex-col w-full justify-center">
+      <div className="flex flex-col justify-center items-center gap-8 p-5">
         {sharedSongs?.map((song: SharedSong) => (
           <ShareItems key={song.postId} song={song}></ShareItems>
         ))}
