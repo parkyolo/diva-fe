5aa06ee3857644bc69d5fd0bf1b458c42e7b1f31
이근학 <o101srb@daum.net>
2024-02-07 13:17:44 +0900

    Feat: 마이페이지 통신

--- frontend/src/containers/mypage/Share/ShareContent.tsx
+++ frontend/src/containers/mypage/Share/ShareContent.tsx
@@ -1,5 +1,5 @@
 import ShareItems from './ShareItems';
-import { SharedSong, Song } from '@/types/song';
+import { SharedSong } from '@/types/song';
 import { useEffect } from 'react';
 import { useFetch } from '@/hooks/useFetch';
 import { req } from '@/services';
