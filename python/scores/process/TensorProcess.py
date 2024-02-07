90e885f0b34a1407395b0af7cb145283367a7843
cha3088 <cha3088@gmail.com>
2024-02-07 09:13:44 +0900

    Fix: TF Limit GPU MEM

--- python/scores/process/TensorProcess.py
+++ python/scores/process/TensorProcess.py
@@ -12,10 +12,9 @@ class TensorProcess(Process):
         os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
         os.environ["CUDA_VISIBLE_DEVICES"] = "6"
 
+    def run(self):
         gpus = tf.config.list_physical_devices('GPU')
         tf.config.set_logical_device_configuration(
             gpus[0],
             [tf.config.LogicalDeviceConfiguration(memory_limit=4096)])
-
-    def run(self):
         self.result_dict['result'] = self.us.analyze()
