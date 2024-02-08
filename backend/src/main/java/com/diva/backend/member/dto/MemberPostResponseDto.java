9660086a08ceb2f917e00121f7cd8aeb0584ab63
최이초 <choeyicho1105@gmail.com>
2024-02-08 14:58:44 +0900

    Refactor: 작성한 포스트 조회 쿼리 개선

--- backend/src/main/java/com/diva/backend/member/dto/MemberPostResponseDto.java
+++ backend/src/main/java/com/diva/backend/member/dto/MemberPostResponseDto.java
@@ -3,8 +3,11 @@ package com.diva.backend.member.dto;
 import com.diva.backend.member.entity.Member;
 import com.diva.backend.post.entity.Post;
 import com.diva.backend.post.entity.PracticeResult;
+import com.diva.backend.song.entity.Song;
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 public class MemberPostResponseDto {
@@ -14,28 +17,31 @@ public class MemberPostResponseDto {
     private final String content;
     private final String songTitle;
     private final String artist;
+    private final Integer score;
     private final String recordUrl;
 
     @Builder
-    public MemberPostResponseDto(Long memberId, Long postId, String nickname, String content, String songTitle, String artist ,String recordUrl) {
+    public MemberPostResponseDto(Long memberId, Long postId, String nickname, String content, String songTitle, String artist, Integer score, String recordUrl) {
         this.memberId = memberId;
         this.postId = postId;
         this.nickname = nickname;
         this.content = content;
         this.songTitle = songTitle;
         this.artist = artist;
+        this.score = score;
         this.recordUrl = recordUrl;
     }
 
-    public static MemberPostResponseDto from (Member member,Post post, PracticeResult practiceResult, String url) {
+    public static MemberPostResponseDto from (Member member,Post post, Song song, Integer score, String recordUrl) {
         return MemberPostResponseDto.builder()
             .memberId(member.getId())
             .postId(post.getId())
             .nickname(member.getNickname())
             .content(post.getContent())
-            .songTitle(practiceResult.getSong().getTitle())
-            .artist(practiceResult.getSong().getArtist())
-            .recordUrl(url)
+            .songTitle(song.getTitle())
+            .artist(song.getArtist())
+            .score(score)
+            .recordUrl(recordUrl)
             .build();
     }
 }
