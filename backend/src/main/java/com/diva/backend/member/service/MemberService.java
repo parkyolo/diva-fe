1eeef1ee09be68fe86b06090ded2a492d78f4f5c
최이초 <choeyicho1105@gmail.com>
2024-02-09 23:10:08 +0900

    Refactor: 회원 정보 수정 시 이미지 파일 S3 업로드

--- backend/src/main/java/com/diva/backend/member/service/MemberService.java
+++ backend/src/main/java/com/diva/backend/member/service/MemberService.java
@@ -1,13 +1,15 @@
 package com.diva.backend.member.service;
 
 import com.diva.backend.member.dto.*;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.util.List;
 
 public interface MemberService {
 
     MemberResponseDto getMemberInfo(String email);
 
-    MemberInfoUpdateResponseDto updateInfo(String email, MemberInfoUpdateRequestDto requestDto);
+    MemberInfoUpdateResponseDto updateInfo(String email, MemberInfoUpdateRequestDto requestDto, MultipartFile file);
 
     List<MemberPostResponseDto> getMemberPosts(String email);
 }
