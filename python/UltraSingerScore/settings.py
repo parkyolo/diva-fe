ac505b804ee7242d90f4221cc73250d7b6295a79
cha3088 <cha3088@gmail.com>
2024-02-14 10:30:04 +0900

    Fix: divamusic.me 추가

--- python/UltraSingerScore/settings.py
+++ python/UltraSingerScore/settings.py
@@ -42,7 +42,7 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "6"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["70.12.130.101", "localhost"]
+ALLOWED_HOSTS = ["70.12.130.101", "localhost", "divamusic.me"]
 
 # Application definition
 
