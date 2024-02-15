3d7a5e5f57f14c7fdbffed623d85700cea606cbb
류기현 <khryu0610@gmail.com>
2024-02-15 14:13:20 +0900

    Feat: 점수 로딩처리, Fix: 부른 노래 정렬 로직 수정

--- frontend/src/containers/range/check/Loader.tsx
+++ frontend/src/containers/range/check/Loader.tsx
@@ -1,6 +1,6 @@
 const Loader: React.FC = () => {
   return (
-    <div className="basis-full flex gap-[12px] justify-center items-center">
+    <div className="h-[100px] flex gap-[12px] justify-center items-center">
       <div className="w-[16px] h-[16px] bg-white rounded-[100px] animate-[loading1_1s_infinite_100ms]"></div>
       <div className="w-[16px] h-[16px] bg-white rounded-[100px] animate-[loading2_1s_infinite_100ms]"></div>
       <div className="w-[16px] h-[16px] bg-white rounded-[100px] animate-[loading3_1s_infinite_100ms]"></div>
