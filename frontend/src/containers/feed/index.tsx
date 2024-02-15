69958902f971d8344e1f9b50a201af2e59568844
박지영 <jiyounge628@gmail.com>
2024-02-15 13:00:45 +0900

    Feat: 피드 무한스크롤, 게시, 수정, 삭제, 좋아요 기능 개발

--- frontend/src/containers/feed/index.tsx
+++ frontend/src/containers/feed/index.tsx
@@ -1,19 +1,22 @@
 'use client';
 
+import Link from 'next/link';
+import PostContainer from './post/PostContainer';
+import UploadButton from './UploadButton';
+import UpdateForm from './UpdateForm';
 import Header from '@/components/Header';
 import Navigation from '@/components/Navigation';
 import UploadForm from '@/containers/feed/UploadForm';
-import { feedPage, updateForm, uploadForm, feedPageAtom } from '@/store/feed';
-import { PostInterface } from '@/types/post';
+import { useEffect, useRef, useState } from 'react';
 import { useAtom } from 'jotai';
-import Link from 'next/link';
-import { useCallback, useEffect, useRef, useState } from 'react';
-import PostContainer from './post/PostContainer';
-import UploadButton from './UploadButton';
+import { feedPage, uploadForm, feedPageAtom } from '@/store/feed';
+import { PostInterface } from '@/types/post';
+import { useFetch } from '@/hooks/useFetch';
+import { req } from '@/services';
 import { HeaderLogo } from '../../../public/svgs';
-import UpdateForm from './UpdateForm';
+import { fetchingAtom, reFetchingAtom } from './store';
 
-const PAGE_SIZE = 5;
+const PAGE_SIZE = 7;
 
 const Feed = () => {
   // 피드 페이지, 업로드 폼 토글
@@ -22,12 +25,54 @@ const Feed = () => {
   // data fetch
   const viewportRef = useRef<HTMLDivElement>(null);
   const throttle = useRef<ReturnType<typeof setTimeout> | null>(null);
-  const [mounted, setMounted] = useState<boolean>(false);
-  const [isFetching, setFetching] = useState(false);
-  const [isLastPage, setLastPage] = useState(false);
+  const [isFetching, setFetching] = useAtom(fetchingAtom);
+  const [isReFetching, setIsReFetching] = useAtom(reFetchingAtom);
+  const [isLastPost, setIsLastPost] = useState<boolean>(false);
+  const [lastPostId, setLastPostId] = useState<number>();
+  const [allPosts, setPosts] = useState<PostInterface[]>([]);
+  const [isLoading, getPosts, error, getAllPosts] = useFetch<PostInterface[]>(
+    req.post.getAllPosts,
+  );
+
+  const clearState = () => {
+    setPosts([]);
+    setLastPostId(undefined);
+    setIsLastPost(false);
+  };
 
   useEffect(() => {
-    setMounted(true);
+    if (isReFetching) {
+      clearState();
+      setFetching(true);
+      setIsReFetching(false);
+    }
+  }, [isReFetching]);
+
+  useEffect(() => {
+    if (getPosts) {
+      if (getPosts.length) {
+        setPosts([...allPosts, ...getPosts]);
+        setLastPostId(getPosts[getPosts.length - 1].postId);
+      } else {
+        setIsLastPost(true);
+      }
+    }
+  }, [getPosts]);
+
+  useEffect(() => {
+    if (isFetching && !isLastPost) {
+      if (!throttle.current) {
+        throttle.current = setTimeout(async () => {
+          getAllPosts({ postId: lastPostId, pageSize: PAGE_SIZE });
+          setFetching(false);
+          throttle.current = null;
+        }, 300);
+      }
+    }
+  }, [isFetching]);
+
+  useEffect(() => {
+    // page 끝 도달 감지
     const handleScroll = () => {
       if (!!viewportRef.current) {
         const { scrollHeight, scrollTop, clientHeight } = viewportRef.current;
@@ -54,14 +99,20 @@ const Feed = () => {
             }
           />
           <main ref={viewportRef}>
-            <PostContainer />
+            {allPosts && allPosts.length ? (
+              <PostContainer allPosts={allPosts} />
+            ) : (
+              <div className="flex flex-col h-full w-full text-xl justify-center items-center">
+                게시글이 없습니다
+              </div>
+            )}
             <UploadButton />
           </main>
           <Navigation />
         </>
-      ) : isFeedPage === uploadForm ?  (
+      ) : isFeedPage === uploadForm ? (
         <UploadForm />
-      ) :  (
+      ) : (
         <UpdateForm />
       )}
     </>
