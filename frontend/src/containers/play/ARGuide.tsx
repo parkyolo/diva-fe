05fa4be01a6e116b6593d59a44348f63a02f36ab
Kihyun Ryu <khryu0610@gmail.com>
2024-02-16 00:32:59 +0900

    Fix: ar mr 싱크 해결

--- frontend/src/containers/play/ARGuide.tsx
+++ frontend/src/containers/play/ARGuide.tsx
@@ -5,15 +5,12 @@ const ARGuide = ({
 }: {
   arAudioRef: RefObject<HTMLAudioElement>;
 }) => {
-  const [isAROn, setAROn] = useState<boolean>(false);
-
   const handleAR = () => {
-    if (isAROn) {
-      if (arAudioRef.current) arAudioRef.current.volume = 0;
+    if (arAudioRef.current?.volume && arAudioRef.current?.volume > 0.1) {
+      if (arAudioRef.current) arAudioRef.current.volume = 0.01;
     } else {
       if (arAudioRef.current) arAudioRef.current.volume = 0.3;
     }
-    setAROn(!isAROn);
   };
 
   return (
@@ -21,7 +18,11 @@ const ARGuide = ({
       onClick={handleAR}
       className="font-samlip text-xl outline-none border shadow-[3px_-3px_10px_1px_rgba(255,255,255,0.3)] rounded-xl w-10/12 py-2 mx-auto"
     >
-      {isAROn ? <span>노래 가이드 끄기</span> : <span>노래 가이드 켜기</span>}
+      {arAudioRef.current && arAudioRef.current.volume > 0.1 ? (
+        <span>노래 가이드 끄기</span>
+      ) : (
+        <span>노래 가이드 켜기</span>
+      )}
     </button>
   );
 };
